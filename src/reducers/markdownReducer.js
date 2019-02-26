9bfc8f4279bda5f327ef7ca1cf9675307ae7ac01
Tyler Corbett <thetylercorbett@gmail.com>
2019-02-26 15:39:58 -0800

    began reducer

---
+++ src/reducers/markdownReducer.js
@@ -0,0 +1,17 @@
+// const initialState = {
+//   markdownText: '#Markdown text'
+// };
+
+// export default function reducer(state = initialState, action) {
+//   switch(action.type) {
+//     case CREATE_TAB:
+//       return {
+//         ...state,
+//         tab:[
+//           ...state.markdownText
+//         ]
+//       };
+//     default: 
+//       return state;
+//   }
+// }
