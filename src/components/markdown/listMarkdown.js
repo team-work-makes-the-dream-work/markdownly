324e720a5dffb72503ff202f10f3d81dcd7abb38
Carmen Perezchica <crperezchica@gmail.com>
2019-02-27 15:32:56 -0800

    inital component setup

---
+++ src/components/markdown/listMarkdown.js
@@ -0,0 +1,19 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+function List({}) {
+
+  return (
+    <>
+    <h1>Markdown List</h1>
+    <ul>
+      <li>{}</li>
+    </ul>
+    </>
+  );
+}
+// List.propTypes = {
+//   list:PropTypes.string.isRequired
+// };
+
+export default List;
