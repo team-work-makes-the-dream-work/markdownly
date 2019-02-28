fc4f501c2e7581d161f6165f9e830f0e4bdb283b
Sophie Graham <sophiergraham1@gmail.com>
2019-02-28 15:24:48 -0800

    added about us text

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,10 @@ export default function aboutPage() {
   return (
     <div>
       <h1>About Us!</h1>
-      <p>Learn more about our team and our business and all you could ever know ever!!</p>
+      <p>Our team cares deeply about your experience with Markdownly! Meet the folks who make it all happen:</p>
+      <p>Tyler C. Software Developer, Time Magazine(&apos;)s Person of the Year 2006 and his parents(&apos;) favorite son!</p> 
+      <p>Sophie G. Software Developer, former Insect Zoo Specialist, and hater of highlighter yellow.</p>
+      <p>Carmen P. Software Developer, Mom to two girls, and our user experience expert!</p>
     </div>
   );
 }
