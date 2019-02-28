0b2794b4b8e5ade7871b4c63ecd6a6572723fd31
Sophie Graham <sophiergraham1@gmail.com>
2019-02-28 15:32:38 -0800

    edited text

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -3,9 +3,8 @@ import React from 'react';
 export default function landingPage() {
   return (
     <div>
-      <h1>Welcome to Our Page!</h1>
-      <p> Spicy jalapeno bacon ipsum dolor amet et eiusmod esse ut hamburger shoulder veniam doner. Chicken hamburger salami deserunt swine meatball. Pork shoulder spare ribs beef, salami pariatur aliquip brisket. Leberkas proident prosciutto ipsum anim, qui ut nulla pork sirloin.
-           olore consectetur est meatball alcatra anim culpa. Bresaola ex adipisicing brisket capicola turducken, aute deserunt tri-tip strip steak venison. Exercitation esse pork loin prosciutto consectetur biltong picanha aliquip alcatra turkey. Consectetur in deserunt est.
+      <h1>Welcome to Markdownly!</h1>
+      <p>Markdownly is  a markdown editor that allows our customers to edit markdown files and see a preview. As a customer, you are able to add and edit tabs, being able to keep track of all your markdown needs!
       </p>
     </div>
   );
