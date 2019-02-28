024365c812f977c09d4f62f01d9259893885ac07
Sophie Graham <sophiergraham1@gmail.com>
2019-02-28 14:21:53 -0800

    hard coded landing page

---
+++ src/components/LandingPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function landingPage() {
+  return (
+    <div>
+      <h1>Welcome to Our Page!</h1>
+      <h3> Spicy jalapeno bacon ipsum dolor amet et eiusmod esse ut hamburger shoulder veniam doner. Chicken hamburger salami deserunt swine meatball. Pork shoulder spare ribs beef, salami pariatur aliquip brisket. Leberkas proident prosciutto ipsum anim, qui ut nulla pork sirloin.
+           olore consectetur est meatball alcatra anim culpa. Bresaola ex adipisicing brisket capicola turducken, aute deserunt tri-tip strip steak venison. Exercitation esse pork loin prosciutto consectetur biltong picanha aliquip alcatra turkey. Consectetur in deserunt est.
+      </h3>
+    </div>
+  );
+}
