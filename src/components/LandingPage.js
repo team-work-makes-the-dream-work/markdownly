197aed8dd2f16f4d5fac904d32cd886282a74ce1
Sophie Graham <sophiergraham1@gmail.com>
2019-02-28 14:37:05 -0800

    changed to p tags

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -4,9 +4,9 @@ export default function landingPage() {
   return (
     <div>
       <h1>Welcome to Our Page!</h1>
-      <h3> Spicy jalapeno bacon ipsum dolor amet et eiusmod esse ut hamburger shoulder veniam doner. Chicken hamburger salami deserunt swine meatball. Pork shoulder spare ribs beef, salami pariatur aliquip brisket. Leberkas proident prosciutto ipsum anim, qui ut nulla pork sirloin.
+      <p> Spicy jalapeno bacon ipsum dolor amet et eiusmod esse ut hamburger shoulder veniam doner. Chicken hamburger salami deserunt swine meatball. Pork shoulder spare ribs beef, salami pariatur aliquip brisket. Leberkas proident prosciutto ipsum anim, qui ut nulla pork sirloin.
            olore consectetur est meatball alcatra anim culpa. Bresaola ex adipisicing brisket capicola turducken, aute deserunt tri-tip strip steak venison. Exercitation esse pork loin prosciutto consectetur biltong picanha aliquip alcatra turkey. Consectetur in deserunt est.
-      </h3>
+      </p>
     </div>
   );
 }
