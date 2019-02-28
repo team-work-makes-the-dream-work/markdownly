3ecaa2a6210dff60bcafa70cb3b17e050830e5bb
Carmen Perezchica <crperezchica@gmail.com>
2019-02-28 13:57:25 -0800

    tabs displaying horizontally

--- src/components/markdown/Tabs.js
+++ src/components/markdown/Tabs.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styles from './Tabs.css';
 
 function Tabs({ markdownTitles }) {
   const markdownList = markdownTitles.map((markdown, i) => {
-    return <li key={i}>{markdown}</li>;
+    return <li key={i} className={styles.Tabs}>{markdown}</li>;
   });
 
   return (
     <>
-      <ul>
+      <ul className={styles.Tabs}>
         {markdownList}
       </ul>
     </>
