11b5dbeb8542721cd9e123fedd3dbb7b83d8002d
Carmen Perezchica <crperezchica@gmail.com>
2019-02-28 15:08:51 -0800

    added eslint ignore to module.exports in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const HtmlPlugin = require('html-webpack-plugin');
 const CleanPlugin = require('clean-webpack-plugin');
 
+// eslint-disable-next-line no-undef
 module.exports = {
   entry: ['@babel/polyfill', './src/index.js'],
   output: {
