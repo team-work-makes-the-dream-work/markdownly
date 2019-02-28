e46557e18bfc96ea15b868cbf38e17e179a95d5b
Carmen Perezchica <crperezchica@gmail.com>
2019-02-28 10:55:31 -0800

    made a few changes to tab file.js

--- src/components/markdown/Tabs.js
+++ src/components/markdown/Tabs.js
@@ -1,22 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function List({ markdown }) {
-  const markdownList = markdown.map((markdown, i) => {
+function Tabs({ markdownFiles }) {
+  const markdownList = markdownFiles.map((markdown, i) => {
     return <li key={i}>{markdown}</li>;
   });
 
   return (
     <>
-    <h1>Markdown List</h1>
     <ul>
       <li>{markdownList}</li>
     </ul>
     </>
   );
 }
-List.propTypes = {
-  markdown:PropTypes.string.isRequired
+Tabs.propTypes = {
+  markdownFiles:PropTypes.string.isRequired
 };
 
-export default List;
+export default Tabs;
