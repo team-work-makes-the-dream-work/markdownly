73d2ab50035654be5fedb72223ed6dbc687385bd
Sophie Graham <sophiergraham1@gmail.com>
2019-02-28 14:30:18 -0800

    created a static about page

---
+++ src/components/About.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function aboutPage() {
+  return (
+    <div>
+      <h1>About Us!</h1>
+      <h3>Learn more about our team and our business and all you could ever know ever!!</h3>
+    </div>
+  );
+}
